bfb8a88ff558a7661d56910a056aa66ed3f71ae9
kymbravin <kymbravin24@gmail.com>
2026-03-20 21:07:54 +0300

    Clean up formatting in main.cpp
    Removed unnecessary whitespace and formatting issues in the code.

--- main.cpp
+++ main.cpp
@@ -9,13 +9,11 @@ int main() {
     const int N = 10000000; // 10 million values
     vector<int> arr(N);
 
-    // Fill with random values
     srand(time(0));
     for (int i = 0; i < N; i++) {
         arr[i] = rand();
     }
 
-    // Exchange algorithm (descending order)
     for (int i = 0; i < N - 1; i++) {
         for (int j = i + 1; j < N; j++) {
             if (arr[i] < arr[j]) {
@@ -26,8 +24,6 @@ int main() {
             }
         }
     }
-
-    // Print first 10 values to verify
     for (int i = 0; i < 10; i++) {
         cout << arr[i] << " ";
     }
